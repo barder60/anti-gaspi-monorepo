42c8c48acf3b0a947fe11b9190e8ff9d7902ab91
barder60 <kaderate.yamamoto@hotmail.fr>
2022-10-11 14:18:32 +0200

    fix(package): remove dist and create calculate package

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,8 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Button } from "ui";
+import { add } from "calculate"
 
 export default function Native() {
+  add(2, 2)
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Native</Text>
